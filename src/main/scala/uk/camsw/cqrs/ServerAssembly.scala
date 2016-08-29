b71c5e5aa5e648d50f6b052688c2a3a03f09c32b
Leon Jones <leonj_24@yahoo.com>
2016-08-29 13:12:52 +0100

    Add actor creation to server assembly

--- src/main/scala/uk/camsw/cqrs/ServerAssembly.scala
+++ src/main/scala/uk/camsw/cqrs/ServerAssembly.scala
@@ -6,5 +6,7 @@ trait ServerAssembly {
 
   implicit val bus = EventBus()
 
+  def withActor[A <: Command[_], B](actor:  Actor[A, B])(implicit tag: ClassTag[A]) = Actor(actor)(bus, tag)
+
   def <<[A <: Command[_]](cmd: A)(implicit tag: ClassTag[A]): List[Event[_]] = bus << cmd
 }
