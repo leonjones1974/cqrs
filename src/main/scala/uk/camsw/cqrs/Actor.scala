e74541fc13945909f5a6b22d4954b9156bebf4e2
Leon Jones <leonj_24@yahoo.com>
2016-11-02 23:35:23 +0000

    Revert "Another experiment"
    This reverts commit c7cc31174f73158caf731bb7376a3622c548b033.

--- src/main/scala/uk/camsw/cqrs/Actor.scala
+++ src/main/scala/uk/camsw/cqrs/Actor.scala
@@ -1,26 +1,11 @@
 package uk.camsw.cqrs
 
-import uk.camsw.cqrs.EventBus.{EmptyEventList, EventList}
-
 import scala.reflect.ClassTag
-import scala.reflect.runtime.universe._
 
-trait Actor[A <: Command[_], B] {
-  def ch(cmd: A)(implicit bus: EventBus): EventList
+trait Actor[A, B] {
+  def ch(cmd: A)(implicit bus: EventBus): List[Event[_]]
 
   val eh: Event[_] => Actor[A, B]
-}
-
-
-abstract class BaseActor[A: TypeTag, B] extends Actor[Command[A], B] {
-  val CmdData = typeTag[A].tpe
-  val % = CmdData
-  type Handler = PartialFunction[Command[A], EventList]
-  type ProducingHandler = EventBus => Handler
-
-  val doNothing: Handler = {
-    case _ => EmptyEventList
-  }
 }
 
 case class ActorHolder[A <: Command[_], B](var actor: Actor[A, B])(implicit bus: EventBus, tag: ClassTag[A]) {
